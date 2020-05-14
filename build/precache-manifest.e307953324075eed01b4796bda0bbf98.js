ad475996827830bfb3957da4aebab74acae2fd92
Jaya <jaya.wired@gmail.com>
2020-05-14 12:06:30 +1000

    aaa

---
+++ build/precache-manifest.e307953324075eed01b4796bda0bbf98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ddff2c7678d0a25de70347e39b9d9cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2faac57668ecab362339",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "88eebd2cf0f6986b7e18",
+    "url": "/static/js/2.f4037ef9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f4037ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2faac57668ecab362339",
+    "url": "/static/js/main.57002b7e.chunk.js"
+  },
+  {
+    "revision": "67944d75d190e9367a20",
+    "url": "/static/js/runtime-main.19398337.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

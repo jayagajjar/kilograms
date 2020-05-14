488ed8a8a072b0a30903f5553f9093716fd35121
Jaya <jaya.wired@gmail.com>
2020-05-14 15:12:00 +1000

    ggg

---
+++ public/sw.js
@@ -0,0 +1,55 @@
+self.addEventListener("install", function(event) {
+    event.waitUntil(preLoad());
+  });
+  
+  var preLoad = function(){
+    console.log("Installing web app");
+    return caches.open("offline").then(function(cache) {
+      console.log("caching index and important routes");
+      return cache.addAll(["../src/index.js"]);
+    });
+  };
+  
+  self.addEventListener("fetch", function(event) {
+    // check if request is made by chrome extensions or web page
+  // if request is made for web page url must contains http.
+  if (!(event.request.url.indexOf('http') === 0)) return; // skip the request. if request is not made with http protocol
+
+    event.respondWith(checkResponse(event.request).catch(function() {
+      return returnFromCache(event.request);
+    }));
+    event.waitUntil(addToCache(event.request));
+  });
+  
+  var checkResponse = function(request){
+    return new Promise(function(fulfill, reject) {
+      fetch(request).then(function(response){
+        if(response.status !== 404) {
+          fulfill(response);
+        } else {
+          reject();
+        }
+      }, reject);
+    });
+  }; 
+  
+  var addToCache = function(request){
+    return caches.open("offline").then(function (cache) {
+      return fetch(request).then(function (response) {
+        console.log(response.url + " was cached");
+        return cache.put(request, response);
+      });
+    });
+  };
+  
+  var returnFromCache = function(request){
+    return caches.open("offline").then(function (cache) {
+      return cache.match(request).then(function (matching) {
+       if(!matching || matching.status == 404) {
+         return cache.match("offline.html");
+       } else {
+         return matching;
+       }
+      });
+    });
+  };
